d7916dc814ccc35ee4a8ccc41f554c802b111e37
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-09-26 13:06:19 -0400

    Hello

--- Guessing_Game/src/GuessingGame.java
+++ Guessing_Game/src/GuessingGame.java
@@ -1,5 +1,56 @@
-
+/*
+ Syeda Islam
+ 9/25/2017
+ */
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.InputMismatchException;
 public class GuessingGame {
-
+	public static void main(String[]args)
+	{
+		Scanner input = new Scanner(System.in);
+		/*System.out.print("What is your name?");
+		String s = input.next();
+		System.out.println("Nice to meet you " + s);
+		System.out.print("How old are you?");
+		int x = input.nextInt();
+		System.out.print("You are " + x + " years old");*/
+		int guess = 0; 
+		boolean isNumber = false;
+		int answer = (int)((Math.random()*10)+1);
+		while(guess != answer)
+		{
+			
+			while(!isNumber)
+			{
+				try
+				{
+					System.out.print("enter your guess");
+				    guess = input.nextInt() ;
+					isNumber = true;
+					if(guess == answer)
+					{
+						System.out.print("You have guessed right!");
+					}
+					else
+					{
+						System.out.print("Sorry,you are wrong");
+					}
+				}
+				catch(InputMismatchException e)
+				
+				{
+					System.out.println("That was error type " + e);
+					System.out.println("That was not a number. Please try again.");
+				}
+			}
+		}
+		
+		input.close();
+		
+	}
+	
+	
 	
 }
+
